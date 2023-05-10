92b2cebc9dc2cbe9f35c28153caa1a59f57a8108
Luis Paulo <luiss_paullo@hotmail.com>
2023-05-09 21:22:34 -0300

    metodos adicionados no service

---
+++ server/src/main/java/io/getarrays/server/service/implementation/ServerServiceImplemantation.java
@@ -0,0 +1,64 @@
+package io.getarrays.server.service.implementation;
+
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import io.getarrays.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImplemantation implements ServerService {
+    private final ServerRepo serverRepo;
+
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
+        return null;
+    }
+
+}
